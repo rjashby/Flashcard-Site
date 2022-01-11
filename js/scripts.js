caebe93a5370e53ddb7aeae4ee87f005826c92c3
Ryan Ashby <rjapsu160@gmail.com>
2022-01-11 10:15:30 -0800

    add toggleclass using this keyword
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-  $("#JavaCard").click(function() {
-    $("#javascriptdefinition").toggle()
-  });
-  $("#operatorcard").click(function() {
-  $("#operatordefinition").toggle()
-  });
-  $("#variablecard").click(function() {
-  $("#variabledefinition").toggle()
+  $('.card').click(function() {
+  $(this).toggleClass('showDef');
   });
 });
